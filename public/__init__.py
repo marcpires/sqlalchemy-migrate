146b1b089139dc2b6cc458915aee81b01b4194bf
Marcelo Rodrigues <marcelo.rodriguespires@gmail.com>
2016-08-12 17:46:05 -0300

    alteração para fazer consultas nos blueprints

--- public/__init__.py
+++ public/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from models import User, db
 
 public = Blueprint('public', __name__)
 
 @public.route("/")
 def index():
-	return render_template("public/index.html")
+	total = User.query.count()
+	return render_template("public/index.html", total=total)
